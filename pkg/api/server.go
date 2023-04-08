0e893744ea0da42f8ebbe25d5a94e2dc74715c65
fazilnbr <fazilkp2000@gmail.com>
2023-04-08 15:46:15 +0530

    setup grpc server
    Here initialized grpc server  and need to setup protoc that do later

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -1,1 +1,26 @@
 package http
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/fazilnbr/banking-grpc-auth-service/pkg/api/handler"
+	"google.golang.org/grpc"
+)
+
+func NewGRPCServer(userHandler *handler.UserHandler, grpcPort string) {
+	lis, err := net.Listen("tcp", ":"+grpcPort)
+	fmt.Println("grpcPort/////", grpcPort)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	// pb.RegisterAuthServiceServer(grpcServer, userHandler)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
