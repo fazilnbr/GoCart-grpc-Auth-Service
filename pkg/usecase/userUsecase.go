99988b7fcc5873bfe1c804233ac4ec8f0de50c55
fazilnbr <fazilkp2000@gmail.com>
2023-04-09 19:03:57 +0530

    userUsecase register
    Here hashed the password

--- pkg/usecase/userUsecase.go
+++ pkg/usecase/userUsecase.go
@@ -1,6 +1,9 @@
 package usecase
 
 import (
+	"crypto/md5"
+	"fmt"
+
 	"github.com/fazilnbr/banking-grpc-auth-service/pkg/domain"
 	repository "github.com/fazilnbr/banking-grpc-auth-service/pkg/repository/interface"
 	interfaces "github.com/fazilnbr/banking-grpc-auth-service/pkg/usecase/interface"
@@ -12,8 +15,22 @@ type userUseCase struct {
 
 // Register implements interfaces.UserUseCase
 func (c *userUseCase) Register(user domain.User) (int, error) {
+	user.Password=HashPassword(user.Password)
 	userId, err := c.userRepo.Register(user)
 	return userId, err
+}
+
+func HashPassword(password string) string {
+	data := []byte(password)
+	password = fmt.Sprintf("%x", md5.Sum(data))
+	return password
+
+}
+
+func VerifyPassword(requestPassword, dbPassword string) bool {
+
+	requestPassword = fmt.Sprintf("%x", md5.Sum([]byte(requestPassword)))
+	return requestPassword == dbPassword
 }
 
 func NewUserUseCase(repo repository.UserRepository) interfaces.UserUseCase {
